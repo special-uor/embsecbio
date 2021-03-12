1ca1647ff3a1de86fa3400048e2975d8e86ffa25
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-03-12 12:56:22 +0000

    Add internal function inspect

--- R/utils-workbooks.R
+++ R/utils-workbooks.R
@@ -1,3 +1,75 @@
+#' Match input data types with the expected ones
+#'
+#' @param x Tibble with the input data.
+#' @param tb String with the table name.
+#' @param coerce Boolean flag to indicate if columns should be coerced to the
+#'     right type.
+#' @param default Numeric value to be used when coercing non-numeric values.
+#'
+#' @return Invisibly return \code{x}
+#' @keywords internal
+inspect <- function(x, tb = class(x)[1], coerce = FALSE, default = -999999) {
+  # Update types of missing data
+  x <- purrr::modify(x, replace_missing)
+
+  # Get types of each column
+  ctypes <- purrr::map_chr(x, typeof)
+
+  # Obtain metadata
+  meta <- rpd()[[tb]]
+  if (is.null(meta))
+    stop("Invalid table name, ", tb, "!", call. = FALSE)
+  # Ignore columns with ID_* prefix
+  meta <- meta[!grepl("ID_", meta$names), ]
+  # Ignore columns with type == "character"
+  meta <- meta[meta$types != "character", ]
+  # Filter columns that are in both the input and DB
+  meta <- meta[meta$names %in% colnames(x), ]
+  # Check the input columns match the expected types
+  idx <- ctypes[meta$names] != meta$types
+
+  # Display columns with wrong data types, if any exists
+  res <- sum(idx, na.rm = TRUE)
+  if (res > 0) {
+    if (coerce) {
+      suppressWarnings({
+        x[, meta$names[idx]] <-
+          purrr::map2_df(x[, meta$names[idx]], meta$types[idx], methods::as) %>%
+          purrr::map2_df(x[, meta$names[idx]],
+                         function(x, y) ifelse(is.na(x) & !is.na(y),
+                                               default, x)
+          )
+      })
+
+      warning("The following column",
+              ifelse(res > 1, "s", ""),
+              " in the table `",
+              tb,
+              "`, ",
+              ifelse(res > 1, "have", "has"),
+              " been coerced to the expected data type: \n",
+              paste0("- ", meta$names[idx], ": ", ctypes[meta$names][idx],
+                     " ---> ", meta$types[idx], collapse = "\n"),
+              "\n",
+              call. = FALSE)
+    } else {
+      stop("The following column",
+           ifelse(res > 1, "s", ""),
+           " in the table `",
+           tb,
+           "`, ",
+           ifelse(res > 1, "have", "has"),
+           " the wrong data type: \n",
+           paste0("- ", meta$names[idx], ": ", ctypes[meta$names][idx], " => ",
+                  meta$types[idx],
+                  collapse = "\n"),
+           "\n",
+           call. = FALSE)
+    }
+  }
+  invisible(x)
+}
+
 #' List workbook sheets
 #'
 #' @return Vector with sheet names.
